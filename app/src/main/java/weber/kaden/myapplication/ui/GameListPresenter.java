a424a9b3f2f3dc56416d4ccb3c326fb9f82aac89
Devon Conger <devon.conger@cognisaya.com>
2018-09-28 12:07:02 -0600

    GameList and Login Presenters
    Started GameList and Login Presenters

--- app/src/main/java/weber/kaden/myapplication/ui/GameListPresenter.java
+++ app/src/main/java/weber/kaden/myapplication/ui/GameListPresenter.java
@@ -1,4 +1,25 @@
 package weber.kaden.myapplication.ui;
 
+import weber.kaden.myapplication.model.ClientFacade;
+
 public class GameListPresenter {
+    private GamesListActivity activity;
+    private ClientFacade client;
+
+    public GameListPresenter(GamesListActivity activity, ClientFacade client) {
+        this.activity = activity;
+        this.client = client;
+    }
+
+//    public List<model.Game>displayGames() {
+//        return client.getGames();
+//    }
+//
+//    public void createGame(String username) throws Exception {
+//        client.createGame(username);
+//    }
+//
+//    public void joinGame(String username) throws Exception {
+//        client.joinGame(username);
+//    }
 }
--- app/src/main/java/weber/kaden/myapplication/ui/LoginPresenter.java
+++ app/src/main/java/weber/kaden/myapplication/ui/LoginPresenter.java
@@ -1,4 +1,44 @@
 package weber.kaden.myapplication.ui;
 
+import weber.kaden.myapplication.model.ClientFacade;
+
 public class LoginPresenter {
+
+    private LoginActivity activity;
+    private ClientFacade client;
+
+    public LoginPresenter(LoginActivity activity, ClientFacade client) {
+        this.activity = activity;
+        this.client = client;
+    }
+
+    public void login(String username, String password) throws Exception {
+         /*
+         receive credentials, pass them to the facade if valid.
+         if facade tells me to destroy, do I destroy? or do I tell view to destroy itself?
+         if facade throws exception, throw exception
+         */
+
+        if (username.isEmpty() || password.isEmpty()) {
+            return;
+        } else {
+            //client.login(username, password); // throws exception if credentials are invalid
+            // either destroy the view or tell the view to destroy itself
+        }
+    }
+
+    public void register(String username, String password) throws Exception {
+        /*
+         receive credentials, pass them to the facade if valid.
+         if facade tells me to destroy, do I destroy? or do I tell view to destroy itself?
+         if facade tells me to throw an exception, tell view to display an error.
+         */
+
+        if (username.isEmpty() || password.isEmpty()) {
+            return;
+        } else {
+            //client.register(username, password); // throws exception if user already exists
+            // either destroy the view or tell the view to destroy itself
+        }
+    }
 }
