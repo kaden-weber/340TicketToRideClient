14388ba452302a9b117aff1660159999ec91d60d
Samuel Fry <samuel.fry13@gmail.com>
2018-10-01 12:39:16 -0600

    Implemented ClientCommunicator

--- app/src/main/java/weber/kaden/myapplication/serverProxy/ClientCommunicator.java
+++ app/src/main/java/weber/kaden/myapplication/serverProxy/ClientCommunicator.java
@@ -1,5 +1,14 @@
 package weber.kaden.myapplication.serverProxy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import weber.kaden.common.Serializer;
 import weber.kaden.common.command.CommandData;
 import weber.kaden.common.Results;
 
@@ -29,6 +38,49 @@ public class ClientCommunicator {
     }
 
     public Results send(CommandData requestInfo, RequestType requestType) {
-        return null;
+    	try {
+		    URL url = new URL("http://" + serverIP + ":" + serverPort + "/" + requestType + "/");
+		    Serializer serializer = new Serializer();
+		    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		    switch (requestType) {
+			    case GET:
+			    	conn.setRequestMethod("GET");
+			    	break;
+			    case POST:
+			    	conn.setRequestMethod("POST");
+			    	break;
+		    }
+		    conn.setRequestProperty("Content-Type", "application/json");
+		    conn.setConnectTimeout(5000);
+
+		    OutputStream reqBody = conn.getOutputStream();
+		    String serializedData = serializer.serializeCommandData(requestInfo);
+		    reqBody.write(serializedData.getBytes());
+		    reqBody.close();
+
+		    conn.connect();
+
+		    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			    InputStream responseBody = conn.getInputStream();
+
+			    BufferedReader in = new BufferedReader(new InputStreamReader(responseBody));
+
+			    String inputLine;
+			    String output = "";
+			    while ((inputLine = in.readLine()) != null) {
+				    output += inputLine;
+			    }
+
+			    responseBody.close();
+			    conn.disconnect();
+
+			    return serializer.deserializeResults(output);
+		    }
+	    }
+	    catch (IOException e) {
+    		e.printStackTrace();
+	    }
+
+	    return null;
     }
 }
