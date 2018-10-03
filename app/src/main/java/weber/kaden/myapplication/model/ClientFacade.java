bfd24a32199261385044c89d7a1f4c4329d45106
Kaden Weber <kweber142@gmail.com>
2018-10-03 12:15:14 -0600

    Revert "client facade implemented"
    This reverts commit 015dd3777bb81102b527b6069aff393b5f5be2de.

--- app/src/main/java/weber/kaden/myapplication/model/ClientFacade.java
+++ app/src/main/java/weber/kaden/myapplication/model/ClientFacade.java
@@ -1,13 +1,5 @@
 package weber.kaden.myapplication.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import weber.kaden.common.command.Command;
-import weber.kaden.common.command.CommandData;
-import weber.kaden.common.command.CommandFactory;
-import weber.kaden.common.command.CommandType;
 import weber.kaden.common.model.Model;
 import weber.kaden.myapplication.serverProxy.ServerProxy;
 import weber.kaden.myapplication.ui.LoginPresenter;
@@ -18,26 +10,14 @@ public class ClientFacade {
     private LoginPresenter presenter;
 
     public boolean login(String username, String password) throws Exception {
-        List<String> credentials = new ArrayList<>(Arrays.asList(username, password));
-        CommandData commandData = new CommandData(credentials, CommandType.LOGIN);
+        ServerProxy.getInstance().login(username, password);
 
-        Command command = CommandFactory.getInstance().getCommand(commandData);
-        command.execute();
-
-        ServerProxy.getInstance().sendCommand(commandData);
-
-        return true;
+        return false;
     }
     public boolean register(String username, String password) throws Exception {
-        List<String> credentials = new ArrayList<>(Arrays.asList(username, password));
-        CommandData commandData = new CommandData(credentials, CommandType.REGISTER);
+        ServerProxy.getInstance().register(username, password);
 
-        Command command = CommandFactory.getInstance().getCommand(commandData);
-        command.execute();
-
-        ServerProxy.getInstance().sendCommand(commandData);
-
-        return true;
+        return false;
     }
 
 }
