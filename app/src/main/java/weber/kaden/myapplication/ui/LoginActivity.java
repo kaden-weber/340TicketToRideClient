37e13c89df8585e33bcd3c6bbb51b334c02aba58
mmolinahn <molina.moisesa@gmail.com>
2018-10-02 21:15:18 -0600

    Added GameList Activity, switches when login is successful

--- app/src/main/java/weber/kaden/myapplication/ui/LoginActivity.java
+++ app/src/main/java/weber/kaden/myapplication/ui/LoginActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -413,7 +414,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                //finish();
+                Intent intent = new Intent(instance, GameListActivity.class);
+                intent.putExtra("USERNAME", mEmail);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
