875e715039c45ff8035db915d4fa9da2ac19598d
zmorty <zack.morty@gmail.com>
2018-10-03 12:21:43 -0600

    Updated Model to behave as an observable

--- common/src/main/java/weber/kaden/common/model/Model.java
+++ common/src/main/java/weber/kaden/common/model/Model.java
@@ -41,7 +41,11 @@ public class Model extends Observable {
         if (this.players.contains(player)) {
             return false;
         }
-        return this.players.add(player);
+        if (this.players.add(player)) {
+            notifyObservers(this.games);
+            return true;
+        }
+        return false;
     }
 
     public boolean hasPlayer(Player player) {return this.players.contains(player); }
@@ -57,38 +61,75 @@ public class Model extends Observable {
     }
 
     public boolean removePlayer(Player player) {
+        if (!this.players.contains(player)) {
+            return false;
+        }
         this.removePlayerFromGames(player);
-        return this.players.remove(player);
+        if (this.players.remove(player)) {
+            notifyObservers(this.games);
+            return true;
+        }
+        return false;
     }
 
     public boolean addPlayerToGame(Player player, Game game) {
+        if (!this.players.contains(player)) {
+            return false;
+        }
         if (game.hasPlayer(player)) {
             return false;
         }
-        return this.games.get(this.games.indexOf(game)).addPlayer(player);
+        if (this.games.get(this.games.indexOf(game)).addPlayer(player)) {
+            notifyObservers(this.games);
+            return true;
+        }
+        return false;
     }
 
     public boolean removePlayerFromGame(Player player, Game game) {
-        return this.games.get(this.games.indexOf(game)).removePlayer(player);
+        if (!game.hasPlayer(player) || !this.players.contains(player)) {
+            return false;
+        }
+        if(this.games.get(this.games.indexOf(game)).removePlayer(player)) {
+            notifyObservers(this.games);
+            return true;
+        }
+        return false;
     }
 
-    public void removePlayerFromGames(Player player) {
+    public boolean removePlayerFromGames(Player player) {
+        if (!this.players.contains(player)) {
+            return false;
+        }
         for (Game game : games) {
             if (game.hasPlayer(player)) {
                 game.removePlayer(player);
             }
         }
+        notifyObservers(this.games);
+        return true;
     }
 
     public boolean addGame(Game game) {
         if (this.games.contains(game)) {
             return false;
         }
-        return this.games.add(game);
+        if (this.games.add(game)) {
+            notifyObservers(this.games);
+            return true;
+        }
+        return false;
     }
 
     public boolean removeGame(Game game) {
-        return this.games.remove(game);
+        if (!this.games.contains(game)) {
+            return false;
+        }
+        if (this.games.remove(game)) {
+            notifyObservers(this.games);
+            return true;
+        }
+        return false;
     }
 
     public Game getGame(String gameID) {
