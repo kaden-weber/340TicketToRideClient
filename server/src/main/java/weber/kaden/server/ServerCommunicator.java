d6d17245a6159d74d0d5576f308be8662af52b93
Kaden Weber <kweber142@gmail.com>
2018-10-03 15:16:21 -0600

    Server cleanup

--- server/src/main/java/weber/kaden/server/ServerCommunicator.java
+++ server/src/main/java/weber/kaden/server/ServerCommunicator.java
@@ -26,8 +26,6 @@ public class ServerCommunicator {
         System.out.println("Starting server");
         httpServer.start();
         System.out.println("Server started");
-        System.out.println(httpServer.getAddress().getHostName());
-
     }
 
     public static void main(String[] args){
