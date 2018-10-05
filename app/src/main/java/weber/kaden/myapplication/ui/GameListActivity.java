cf82d8c0470c0176345b65f883088f2c9879a692
mmolinahn <molina.moisesa@gmail.com>
2018-10-05 17:57:35 -0600

    RecyclerView Working to display game lists on GameListActivity

--- app/src/main/java/weber/kaden/myapplication/ui/GameListActivity.java
+++ app/src/main/java/weber/kaden/myapplication/ui/GameListActivity.java
@@ -2,21 +2,43 @@ package weber.kaden.myapplication.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import weber.kaden.myapplication.R;
 import weber.kaden.myapplication.model.ClientFacade;
 
-public class GameListActivity extends AppCompatActivity {
+public class GameListActivity extends AppCompatActivity implements GameListAdapter.ItemClickListener{
     private GameListActivity instance = this;
     private ClientFacade clientFacade = new ClientFacade();
+    GameListAdapter adapter;
     GameListPresenter gameListPresenter = new GameListPresenter(instance, clientFacade);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
-        //gameListPresenter.getGame()
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Game 1");
+        animalNames.add("Game 2");
+        animalNames.add("Game 3");
+        animalNames.add("Game 4");
 
-
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.gamelist_layout);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new GameListAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
+
 }
+
