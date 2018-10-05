49444e2101cb8fbfa4893e68e1fd643125bf0119
Kaden Weber <kweber142@gmail.com>
2018-10-05 17:42:41 -0600

    implementing iCommandManager

--- server/src/main/java/weber/kaden/server/CommandManager.java
+++ server/src/main/java/weber/kaden/server/CommandManager.java
@@ -7,8 +7,11 @@ import weber.kaden.common.Results;
 import weber.kaden.common.command.Command;
 import weber.kaden.common.command.CommandData;
 import weber.kaden.common.command.CommandFactory;
+import weber.kaden.common.command.iCommandManager;
+import weber.kaden.common.model.Game;
+import weber.kaden.common.model.Model;
 
-public class CommandManager {
+public class CommandManager implements iCommandManager {
 
     private static CommandManager sCommandManager;
     public static CommandManager getInstance(){
@@ -30,6 +33,25 @@ public class CommandManager {
         return command.execute();
     }
 
+    @Override
+    public Results gamesList() {
+        List<Game> games = Model.getInstance().getGames();
+        return new Results(games, true, null);
+    }
+
+    @Override
+    public Results gameLobby() {
+
+        return null;
+    }
+
+    @Override
+    public Results game() {
+        //Game game = Model.getInstance().getGame()
+        return null;
+    }
+
+// return games list and current game
 
 
 }
