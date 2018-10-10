e9cc7c608467a37fed4b69bc765ed2f4f16f87b6
Samuel Fry <samuel.fry13@gmail.com>
2018-10-10 00:13:01 -0600

    Can't start game if it's already started

--- common/src/main/java/weber/kaden/common/model/Game.java
+++ common/src/main/java/weber/kaden/common/model/Game.java
@@ -117,7 +117,7 @@ public class Game {
     }
 
     public boolean start() {
-        if (this.getPlayers().size() < 2 || this.getPlayers().size() > 5) {
+        if (this.getPlayers().size() < 2 || this.getPlayers().size() > 5 || this.isStarted()) {
             return false;
         }
         setStarted(true);
