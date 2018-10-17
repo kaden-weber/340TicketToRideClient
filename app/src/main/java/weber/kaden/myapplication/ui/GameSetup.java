9d0607548dc745991fe27d4f6d9528ea12ea6160
mmolinahn <molina.moisesa@gmail.com>
2018-10-17 10:59:24 -0600

    Adding Views and Fragments

---
+++ app/src/main/java/weber/kaden/myapplication/ui/GameSetup.java
@@ -0,0 +1,15 @@
+package weber.kaden.myapplication.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import weber.kaden.myapplication.R;
+
+public class GameSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_setup);
+    }
+}
