3252e725dfb3ca3a0035d895bc5b11ebb1fb7a43
Samuel Fry <samuel.fry13@gmail.com>
2018-10-29 19:45:06 -0600

    Added methods for getting cards

--- app/src/main/java/weber/kaden/myapplication/ui/GamePresenter.java
+++ app/src/main/java/weber/kaden/myapplication/ui/GamePresenter.java
@@ -1,5 +1,9 @@
 package weber.kaden.myapplication.ui;
 
+import java.util.List;
+
+import weber.kaden.common.model.DestinationCard;
+import weber.kaden.common.model.TrainCard;
 import weber.kaden.myapplication.model.ClientFacade;
 
 public class GamePresenter {
@@ -9,5 +13,35 @@ public class GamePresenter {
     public GamePresenter(GameViewInterface view, ClientFacade client) {
         this.view = view;
         this.client = client;
+    }
+
+    public List<TrainCard> getFaceUpTrainCards() {
+
+    	return null;
+    }
+
+    public List<TrainCard> getTrainCardsDeck() {
+
+    	return null;
+    }
+
+    public boolean drawFaceUpTrainCard(TrainCard drawnCard) {
+
+    	return false;
+    }
+
+    public boolean drawTrainCardFromDeck() {
+
+    	return false;
+    }
+
+    public List<DestinationCard> getDrawableDestinationCards() {
+
+    	return null;
+    }
+
+    public boolean chooseDestinationCards(List<DestinationCard> drawnCards) {
+
+    	return false;
     }
 }
