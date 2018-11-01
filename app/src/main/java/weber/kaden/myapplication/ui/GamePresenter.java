b47d9cb75847ebcba338a0ea0bbeeb906f7eb66b
Kaden Weber <kweber142@gmail.com>
2018-10-31 19:34:42 -0600

    game testing methods added
    also made a route assign it's own score.

--- app/src/main/java/weber/kaden/myapplication/ui/GamePresenter.java
+++ app/src/main/java/weber/kaden/myapplication/ui/GamePresenter.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+import weber.kaden.common.model.City;
 import weber.kaden.common.model.DestinationCard;
 import weber.kaden.common.model.Model;
+import weber.kaden.common.model.Route;
 import weber.kaden.common.model.TrainCard;
+import weber.kaden.common.model.TrainCardType;
 import weber.kaden.myapplication.model.ClientFacade;
 
 public class GamePresenter implements Observer {
@@ -50,13 +53,46 @@ public class GamePresenter implements Observer {
 
     @Override
     public void update(Observable observable, Object o) {
-        //Idk what to do here
+        //update claimed routes
 
     }
 
     public void runPhase2Test() {
         //TODO Run test list
+        //TODO: Run in an asynctask so there can be pauses?
         view.sendMessage("Running Tests");
+
+        view.sendMessage("Adding to your train cards");
+        //client.drawTrainCard(client.getCurrentUser());
+        //delay
+        view.sendMessage("Removing one of your train cards");
+//        client.removeTrainCardFromPlayer(client.getCurrentUser()); //??
+        view.sendMessage("Adding to your destination cards");
+//        client.drawDestinationCard(client.getCurrentUser());
+        view.sendMessage("Removing one of your destination cards");
+//        client.removeDestinationCardFromPlayer(client.getCurrentUser()); //??
+        view.sendMessage("Updating opponent train cards");
+//        client.dealTrainCardsToOpponents();
+        view.sendMessage("Updating opponent train cars");
+//        client.changeOpponentsTrainCars();
+        view.sendMessage("Updating opponent destination cards");
+//        client.dealDestinationCardsToOpponents();
+        view.sendMessage("Updating number of visible train cards in deck");
+        //don't know what to do for these
+        view.sendMessage("Updating number of invisible train cards in deck");
+        //ditto
+        view.sendMessage("Updating number of destination cards in deck");
+        //ditto
+        view.sendMessage("Adding a claimed route: Calgary-Winnipeg");
+//        client.haveOpponentClaimRoute(new Route(City.CALGARY, City.WINNIPEG, 6, TrainCardType.PASSENGER));
+        view.sendMessage("Adding a chat message");
+//        ChatPresenter chatPresenter = new ChatPresenter(mock, ); // Looks like we need mocks for this
+//        chatPresenter.sendMessage("Hey guys!");
+
+        view.sendMessage("Advancing turn to next player");
+//        client.endTurn(client.getCurrentUser());
+
+
 //  For each change, the presenter should:
 //  o Call the view to display a toast describing the change (e.g., “Updating player points”).
 //  o Make the indicated change to the local client model (no server interaction is
