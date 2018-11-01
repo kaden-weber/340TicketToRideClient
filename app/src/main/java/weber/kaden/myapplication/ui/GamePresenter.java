1cb0b9f1cb303771330992dcfed99d2aed66fd73
Kaden Weber <kweber142@gmail.com>
2018-10-31 18:54:55 -0600

    game presenter test notes

--- app/src/main/java/weber/kaden/myapplication/ui/GamePresenter.java
+++ app/src/main/java/weber/kaden/myapplication/ui/GamePresenter.java
@@ -57,5 +57,30 @@ public class GamePresenter implements Observer {
     public void runPhase2Test() {
         //TODO Run test list
         view.sendMessage("Running Tests");
+//  For each change, the presenter should:
+//  o Call the view to display a toast describing the change (e.g., “Updating player points”).
+//  o Make the indicated change to the local client model (no server interaction is
+//        involved here). This should cause your client model to notify presenters of
+//        the model changes, and presenters should update their views appropriately.
+//  o The following items should be demonstrated:
+//      ● Update player points
+//      ● Add/remove train cards for this player
+//      ● Add/remove player destination cards for this player
+//      ● Update the number of train cards for opponent players
+//      ● Update the number of train cars for opponent players
+//      ● Update the number of destination cards for opponent players
+//      ● Update the visible (face up) cards in the train card deck
+//      ● Update the number of invisible (face down) cards in train card deck
+//      ● Update the number of cards in destination card deck
+//      ● Add claimed route (for any player). Show this on the map.
+//      ● Add chat message from any player
+//      ● Advance player turn (change the turn indicator so it indicates another
+//                player)
+//  o Include pauses as needed to allow the TA to see each change.
+//  o Each of the capabilities must be implemented by modifying the client model
+//        which in return uses the observable to notify the presenters (controllers),
+//                which update the views.
+
+
     }
 }
