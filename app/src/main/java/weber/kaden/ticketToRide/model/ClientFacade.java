e5e2d5ff0afde8ce89cc4b8fc01a086486a0b10f
Devon Conger <devon.conger@cognisaya.com>
2018-11-13 15:21:11 -0700

    serializing fix

--- app/src/main/java/weber/kaden/ticketToRide/model/ClientFacade.java
+++ app/src/main/java/weber/kaden/ticketToRide/model/ClientFacade.java
@@ -7,6 +7,9 @@ import java.util.List;
 import weber.kaden.common.Results;
 import weber.kaden.common.command.Command;
 import weber.kaden.common.command.CommandData.CommandData;
+import weber.kaden.common.command.CommandData.CommandDataClaimRoute;
+import weber.kaden.common.command.CommandData.CommandDataDrawDestinationCards;
+import weber.kaden.common.command.CommandData.CommandDataDrawTrainCardFromFaceUp;
 import weber.kaden.common.command.CommandFactory;
 import weber.kaden.common.command.CommandType;
 import weber.kaden.common.model.DestinationCard;
@@ -173,9 +176,7 @@ public class ClientFacade {
 
     public void claimRoute(String gameId, String userId, Route route) throws Exception {
         List<String> params = new ArrayList<>((Arrays.asList(gameId, userId)));
-        List<Object> data = new ArrayList<>();
-        data.add(route);
-        CommandData commandData = new CommandData(params, CommandType.CLAIMROUTE, data);
+        CommandData commandData = new CommandDataClaimRoute(params, CommandType.CLAIMROUTE, route);
         Results results = ServerProxy.getInstance().sendCommand(commandData);
         if(!results.success()){
             throw new Exception(results.getErrorInfo());
@@ -188,10 +189,8 @@ public class ClientFacade {
 
     public void sendDestinationCards(String playerId, String gameId, List<DestinationCard> keptCards, List<DestinationCard> discarded) throws Exception{
         List<String> params = new ArrayList<>((Arrays.asList(gameId, playerId)));
-        List<Object> data = new ArrayList<>();
-        data.add(keptCards);
-        data.add(discarded);
-        CommandData commandData = new CommandData(params, CommandType.DRAWDESTINATIONCARDS, data);
+        CommandData commandData = new CommandDataDrawDestinationCards(params, CommandType.DRAWDESTINATIONCARDS,
+                keptCards, discarded);
         Results results = ServerProxy.getInstance().sendCommand(commandData);
         if(!results.success()){
             throw new Exception(results.getErrorInfo());
@@ -202,7 +201,8 @@ public class ClientFacade {
         List<String> params = new ArrayList<>(Arrays.asList(gameId, playerId));
         List<Object> data = new ArrayList<>();
         data.add(cardIndex);
-        CommandData commandData = new CommandData(params, CommandType.DRAWTRAINCARDFROMFACEUP, data);
+        CommandData commandData = new CommandDataDrawTrainCardFromFaceUp(params,
+                CommandType.DRAWTRAINCARDFROMFACEUP, cardIndex);
         Results results = ServerProxy.getInstance().sendCommand(commandData);
         if (!results.success()) {
             throw new Exception(results.getErrorInfo());
--- common/src/main/java/weber/kaden/common/Serializer.java
+++ common/src/main/java/weber/kaden/common/Serializer.java
@@ -3,6 +3,10 @@ package weber.kaden.common;
 import com.google.gson.Gson;
 
 import weber.kaden.common.command.CommandData.CommandData;
+import weber.kaden.common.command.CommandData.CommandDataClaimRoute;
+import weber.kaden.common.command.CommandData.CommandDataDiscardTrainCard;
+import weber.kaden.common.command.CommandData.CommandDataDrawDestinationCards;
+import weber.kaden.common.command.CommandData.CommandDataDrawTrainCardFromFaceUp;
 import weber.kaden.common.command.CommandType;
 
 public class Serializer {
@@ -15,7 +19,20 @@ public class Serializer {
         return gson.toJson(data);
     }
 
-    public CommandData deserializeCommandData(String data) {return gson.fromJson(data, CommandData.class); }
+    public CommandData deserializeCommandData(String data) {
+        CommandData tempData = gson.fromJson(data, CommandData.class);
+        switch (tempData.getType()) {
+            case DRAWDESTINATIONCARDS:
+                return gson.fromJson(data, CommandDataDrawDestinationCards.class);
+            case CLAIMROUTE:
+                return gson.fromJson(data, CommandDataClaimRoute.class);
+            case DISCARDTRAINCARD:
+                return gson.fromJson(data, CommandDataDiscardTrainCard.class);
+            case DRAWTRAINCARDFROMFACEUP:
+                return gson.fromJson(data, CommandDataDrawTrainCardFromFaceUp.class);
+        }
+        return gson.fromJson(data, CommandData.class);
+    }
 
     public Results deserializeResults(String string, CommandType type) {
         switch (type) {
