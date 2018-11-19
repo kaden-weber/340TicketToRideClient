be6912e94183584cb3c840e778cd7c9923d4b0b3
Samuel Fry <samuel.fry13@gmail.com>
2018-11-19 09:36:50 -0700

    Game Over Activity Structure

---
+++ app/src/main/java/weber/kaden/ticketToRide/ui/gameOver/GameOverActivity.java
@@ -0,0 +1,15 @@
+package weber.kaden.ticketToRide.ui.gameOver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import weber.kaden.ticketToRide.R;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+    }
+}
