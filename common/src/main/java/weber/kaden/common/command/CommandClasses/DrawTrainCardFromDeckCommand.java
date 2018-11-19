98483d8fd03c88712234901c3ed262b1adf23842
mmolinahn <molina.moisesa@gmail.com>
2018-11-19 12:13:14 -0700

    Test for train cards end turn

--- common/src/main/java/weber/kaden/common/command/CommandClasses/DrawTrainCardFromDeckCommand.java
+++ common/src/main/java/weber/kaden/common/command/CommandClasses/DrawTrainCardFromDeckCommand.java
@@ -21,7 +21,7 @@ public class DrawTrainCardFromDeckCommand implements Command {
     @Override
     public Results execute() {
         if (Model.getInstance().getGame(gameID).PlayerDrawTrainCardFromDeck(playerID)) {
-            Model.getInstance().getGame(gameID).finishTurn();
+            //Model.getInstance().getGame(gameID).finishTurn();
             return new GameResults(null, true, null);
         }
         else {
