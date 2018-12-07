a3f5478e5a351ab1fba98cb893028bc707de5610
zmorty <zack.morty@gmail.com>
2018-12-07 12:45:47 -0700

    SQLDaoFactory

--- SQL/src/main/java/com/example/sql/SQLDaoFactory.java
+++ SQL/src/main/java/com/example/sql/SQLDaoFactory.java
@@ -3,43 +3,88 @@ package com.example.sql;
 import java.util.List;
 
 import weber.kaden.common.command.CommandData.CommandData;
+import weber.kaden.common.injectedInterfaces.persistence.CommandDataDao;
 import weber.kaden.common.injectedInterfaces.persistence.DaoFactory;
+import weber.kaden.common.injectedInterfaces.persistence.GameDao;
+import weber.kaden.common.injectedInterfaces.persistence.UserDao;
 import weber.kaden.common.model.Game;
 import weber.kaden.common.model.Player;
 
 public class SQLDaoFactory implements DaoFactory {
+
+    private void createconnection() {
+        return;
+    }
+
+    private void closeConnection(Sqlconnection) {
+        Sqlconnection.close();
+        return;
+    }
+
+    private void clearConnection(SQLconnection) {
+        SQLConnection.clear();
+        SQLConnection.rollback();
+        return;
+    }
+
     @Override
     public boolean saveUsers(List<Player> users) {
-        return false;
+        // connection = createConnection
+        try {
+            UserDao userDao = new SQLUserDao();//connection);
+            if (userDao.save(users)) {
+                return true;
+            }
+            return false;
+            closeConnection();
+        } catch (Exception e) {
+            clearConnection();
+            closeConnection();
+        }
     }
 
     @Override
     public boolean saveGames(List<Game> games) {
+        GameDao gameDao = new SQLGameDao();
+        if (gameDao.save(games)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean clearCommandDeltas() {
+        CommandDataDao commandDataDao = new SQLCommandDataDao();
+        if (commandDataDao.clear()) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public List<Player> getUsers() {
-        return null;
+        UserDao userDao = new SQLUserDao();
+        return userDao.getUsers();
     }
 
     @Override
     public List<Game> getGames() {
-        return null;
+        GameDao gameDao = new SQLGameDao();
+        return gameDao.getGames();
     }
 
     @Override
     public List<CommandData> getCommands() {
-        return null;
+        CommandDataDao commandDataDao = new SQLCommandDataDao();
+        return commandDataDao.getCommands();
     }
 
     @Override
     public boolean addCommandData(CommandData commandData) {
+        CommandDataDao commandDataDao = new SQLCommandDataDao();
+        if (commandDataDao.add(commandData)) {
+            return true;
+        }
         return false;
     }
 }
