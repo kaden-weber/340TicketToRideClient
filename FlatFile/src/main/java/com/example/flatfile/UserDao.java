0b9a2b0067417673334c48a1e18859862a1b5987
Devon Conger <devon.conger@cognisaya.com>
2018-12-12 12:21:54 -0700

    flatFile implementation

--- FlatFile/src/main/java/com/example/flatfile/UserDao.java
+++ FlatFile/src/main/java/com/example/flatfile/UserDao.java
@@ -1,6 +1,11 @@
 package com.example.flatfile;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import weber.kaden.common.model.Player;
@@ -9,24 +14,51 @@ public class UserDao implements weber.kaden.common.injectedInterfaces.persistenc
 
     private String filePath;
     private File file;
+    private Serializer serializer;
 
     UserDao(String filePath) {
+        serializer = new Serializer();
         this.filePath = filePath;
         file = new File(filePath);
         try {
             file.createNewFile();
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 
     @Override
     public boolean save(List<Player> users) {
-        return false;
+        String serializedData = serializer.serialize(users);
+        // write to file
+        BufferedWriter writer = null;
+        boolean success = false;
+        try {
+            writer = new BufferedWriter(new FileWriter(file));
+            writer.write(serializedData);
+            success = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(writer != null)
+                    writer.close();
+            } catch (Exception e) {
+                System.out.println("Error in closing the BufferedWriter" + e);
+                success = false;
+            }
+        }
+        return success;
     }
 
     @Override
     public List<Player> getUsers() {
+        try {
+            String serializedData = new String(Files.readAllBytes(Paths.get(filePath)));
+            return serializer.deserializePlayers(serializedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
