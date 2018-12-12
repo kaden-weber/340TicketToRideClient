d5726c85e8c633eaa29416575b11511715145631
mmolinahn <molina.moisesa@gmail.com>
2018-12-12 12:19:22 -0700

    CommandDAO for flat

--- FlatFile/src/main/java/com/example/flatfile/CommandDataDao.java
+++ FlatFile/src/main/java/com/example/flatfile/CommandDataDao.java
@@ -2,10 +2,14 @@ package com.example.flatfile;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import weber.kaden.common.command.Command;
 import weber.kaden.common.command.CommandData.CommandData;
 
 public class CommandDataDao implements weber.kaden.common.injectedInterfaces.persistence.CommandDataDao {
@@ -55,14 +59,51 @@ public class CommandDataDao implements weber.kaden.common.injectedInterfaces.per
         // just delete the file completely
         return file.delete();
     }
+    private String readFile(String pathname) throws IOException {
 
+        File file = new File(pathname);
+        StringBuilder fileContents = new StringBuilder((int)file.length());
+
+        try (Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNextLine()) {
+                fileContents.append(scanner.nextLine() + System.lineSeparator());
+            }
+            return fileContents.toString();
+        }
+    }
     @Override
     public boolean add(CommandData data) {
+        //open file
+        try {
+            String contents = readFile("commands.json");
+            List<CommandData> currentData = serializer.deserializeCommandData(contents);
+            currentData.add(data);
+            File f = new File("commands.json");
+            f.delete();
+            save(currentData);
+
+        } catch (Exception e){
+            System.out.println("File not created yet");
+            List<CommandData> currentData = new ArrayList<>();
+            currentData.add(data);
+            save(currentData);
+        }
+
         return false;
     }
 
     @Override
     public List<CommandData> getCommands() {
-        return null;
+        try {
+            String contents = readFile("commands.json");
+            List<CommandData> currentData = serializer.deserializeCommandData(contents);
+            return  currentData;
+
+
+        } catch (Exception e){
+            System.out.println("File not created yet");
+            List<CommandData> currentData = new ArrayList<>();
+            return  currentData;
+        }
     }
 }
